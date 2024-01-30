4a0ac6a3285d8714b262221a48604969c1441f1c
post-coder <gabrielspanu.dev@gmail.com>
2024-01-30 12:17:59 +0100

    esempio lampadina

--- js/script.js
+++ js/script.js
@@ -5,21 +5,41 @@ const buttonElement = document.querySelector("#pulsante");
 
 console.log(buttonElement)
 
-// ok, ora rimani in attesa di un evento, nello specifico il "click"
-// "aggiungere un ascoltatore di eventi"
-buttonElement.addEventListener("click", 
+
+// il pulsante è collegato ad una specie di lampadina
+// se la lampadina è spenta e premiamo il pulsante, si accende e viceversa
+
+// la lampadina è accesa
+let lampadinaAccesa = true;
+
+
+
+buttonElement.addEventListener('mouseleave',
     function() {
-        // tutto quello che c'è scritto dentro la function(),
-        // verrà eseguito SOLO quando si verifica l'evento.
-        // l'addEventListener NON BLOCCA L'ESECUZIONE DEL RESTO DEL CODICE
 
-        console.log("click del pulsante");
+        // se la lampada è spenta, accendila
+        // viceversa
 
-        titleElement.style.color = "red";
-        titleElement.innerText = "Titolo cambiato";
+        if (lampadinaAccesa) {
 
-        document.querySelector("#likeNumber").innerText = Number(document.querySelector("#likeNumber").innerText) + 1;
+            lampadinaAccesa = false;
+
+            // cambio il testo del titolo
+            titleElement.innerText = "Lampadina spenta"; 
+            titleElement.className = "spenta";
+
+            console.log(lampadinaAccesa) 
+
+
+        } else {
+
+            lampadinaAccesa = true;
+
+            titleElement.innerText = "Lampadina accesa";
+            titleElement.className = "accesa";
+
+            console.log(lampadinaAccesa);
+        }
+
     }
 )
-
-console.log("dopo l'event listener");
