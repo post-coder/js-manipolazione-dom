bdd3bb0d7e82f55976e01817dbca74629056be99
post-coder <gabrielspanu.dev@gmail.com>
2024-01-30 12:00:24 +0100

    event listener!

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 
 const titleElement = document.querySelector("h1");
 
-// console.log(titleElement);
+const buttonElement = document.querySelector("#pulsante");
 
-// titleElement.className = titleElement.className + " big";
+console.log(buttonElement)
 
-titleElement.className += " big";
+// ok, ora rimani in attesa di un evento, nello specifico il "click"
+// "aggiungere un ascoltatore di eventi"
+buttonElement.addEventListener("click", 
+    function() {
+        // tutto quello che c'è scritto dentro la function(),
+        // verrà eseguito SOLO quando si verifica l'evento.
+        // l'addEventListener NON BLOCCA L'ESECUZIONE DEL RESTO DEL CODICE
 
+        console.log("click del pulsante");
 
+        titleElement.style.color = "red";
+        titleElement.innerText = "Titolo cambiato";
+
+        document.querySelector("#likeNumber").innerText = Number(document.querySelector("#likeNumber").innerText) + 1;
+    }
+)
+
+console.log("dopo l'event listener");
